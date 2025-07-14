dcea97c65d4d2f4d42e91ca1ad2886f6d648f6e9
RD12 <smaldalbahy@gmail.com>
2025-07-14 22:35:07 +0300

    the file is modified now

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from hap!")
+import pandas as pd
 
+def main():
+    
+    df = pd.read_csv("data.csv")
+    print("=================================")
+    print(" Dataset Summary ")
+    print("=================================")
+    print(df.info())
+    print("=================================")
+    print("\n First 10 Records ")
+    print("=================================")
+    print(df.head(10))
+    print("=================================")
+    print("\n Missing Values Check")
+    print(df.isnull().sum())
+    print("=================================")
+    print("\n Summary Statistics")
+    print(df.describe(include='all'))
+    print("=================================")
 
 if __name__ == "__main__":
     main()
